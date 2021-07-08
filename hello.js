238baffca0f9e6dfb5849d91c56e873ba2c72e78
Hasan Lam <hcb.lam@gmail.com>
2021-07-08 03:25:51 +0000

    Exercises for isEven function

--- hello.js
+++ hello.js
@@ -5,8 +5,19 @@ const sayHello = function() {
 sayHello();
 */
 
-
+/*
 const sayHello = function(name) {
   console.log('Hello, ' + name);
 }
 sayHello('Caliban');
+*/
+
+const sayHelloToConsole = function (name) {
+  console.log('Hello, ' + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+const greeting = returnSayHello('John');
